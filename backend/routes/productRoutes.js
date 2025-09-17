f472ca7f1d0df610f1895a67446dae0457b151d8
luckytsori8@gmail.com <luckytsori8@gmail.com>
2025-09-18 00:58:25 +0300

    working on the reviewLIst page and als the productController and adminSlice file

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,31 @@
 import express from 'express';
 import {
-    createProduct, getAllProducts, getSingleProduct, updateProduct, deleteProduct,
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
     getAdminProducts,
     creatingProductReviews,
     getProductReviews,
     deleteProductReview
 } from '../controllers/productController.js';
+
 import { roleBasedAccess, verifyUserAuth } from '../middleware/userAuth.js';
 
 const router = express.Router();
 
-
 // Public Routes
 router.get('/products', getAllProducts);
 router.get('/product/:id', getSingleProduct);
-router.get('/reviews', getProductReviews);
 router.put('/review', verifyUserAuth, creatingProductReviews);
-router.delete('/reviews', verifyUserAuth, deleteProductReview);
-
 
 // Admin Routes
 router.get('/admin/products', verifyUserAuth, roleBasedAccess("admin"), getAdminProducts);
+router.get('/admin/reviews', verifyUserAuth, roleBasedAccess("admin"), getProductReviews);
 router.post('/admin/product/create', verifyUserAuth, roleBasedAccess("admin"), createProduct);
 router.put('/admin/product/:id', verifyUserAuth, roleBasedAccess("admin"), updateProduct);
+router.delete('/admin/reviews', verifyUserAuth, roleBasedAccess("admin"), deleteProductReview);
 router.delete('/admin/product/:id', verifyUserAuth, roleBasedAccess("admin"), deleteProduct);
 
 export default router;
